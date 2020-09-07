583a4091c10238c72bd39fd10dd01fe008e1d195
narratives-study-georgia <70898512+narratives-study-georgia@users.noreply.github.com>
2020-09-07 17:35:14 +0400

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,13 +1,58 @@
 var step = false
 var basis = false
 
-var graphData, allValues, tmp
+var graphData, allValues
+
+var color = (d, i) => {
+    var radios = document.getElementsByName('coloryze')[0];
+    var selected = radios.value;
+    
+    if(!selected || selected == 'None' ) return '#747474';
+    
+    colls = {};
+
+    [...drowDropdowns[selected].dom.getElementsByClassName('select-option')].forEach(opt => colls[opt.innerText] = window.getComputedStyle(opt).color )
+    
+    if(!d.data)  return '#747474';
+    
+    // console.log(colls[d.data[selected]])
+    return colls[d.data[selected]] //| '#747474'
+    return "rgb(148, 103, 189)"
+}
+
+var drowDots = (__data) => {
+    var line1 = document.getElementsByClassName('line-1')[0];
+    var line2 = document.getElementsByClassName('line-2')[0];
+    line1.innerHtml = '';
+    line2.innerHtml = '';
+    __data.forEach(row => {
+        if(row.data["In this situation you…-Did not get personally involved"]){
+            var dotOnLine = document.createElement('i')
+            dotOnLine.setAttribute('nar-id', row.data.FragmentID)
+            dotOnLine.setAttribute('class', 'dot-on-line')
+            dotOnLine.setAttribute('style', 'left: ' + (+row.data["In this situation you…-Did not get personally involved"] * 4)+ 'px; border-color: ' + color(row))
+            line1.appendChild(dotOnLine)
+            // console.log(row.data["In this situation you…-Did not get personally involved"])
+        }
+        if(row.data["In this situation you…-Acted gradually"]){
+            var dotOnLine = document.createElement('i')
+            dotOnLine.setAttribute('style', 'left: ' + (+row.data["In this situation you…-Acted gradually"] * 4)+ 'px; border-color: ' + color(row))
+            dotOnLine.setAttribute('class', 'dot-on-line')
+            dotOnLine.setAttribute('nar-id', row.data.FragmentID)
+            line2.appendChild(dotOnLine)
+            // console.log(row.data["In this situation you…-Did not get personally involved"])
+        }
+    })
+}
+
 var drow = (data_, linetype) => {
+    // if(!data) return;
+    // drowDots(data_) 
     var data = data_
     graphData = data_
     d3.select("svg").remove();
     var width = window.innerWidth - 100;
-    var height = window.innerHeight / 2;
+    var height = 350//window.innerHeight / 2;
     document.getElementById('count').innerText = data_.length
    /* Scale */
     var xScale = d3.scaleTime()
@@ -18,21 +63,8 @@ var drow = (data_, linetype) => {
         .domain([100, 0])
         .range([0, height]);
     
-    var colorArr = d3.scaleOrdinal(d3.schemeCategory10);
-    var color = (d, i) => {
-        var radios = document.getElementsByName('coloryze')[0];
-        var selected = radios.value;
-        
-        if(!selected || selected == 'None' ) return '#747474';
-        var arr = []
-        for(var l = 0; l < drowDropdowns[selected].options.length; l++){
-            arr.push(drowDropdowns[selected].options[l].value)
-        }
-        if(!d.data)  return '#747474';
-        tmp = [arr, colorArr(arr.indexOf(d.data[selected])), arr.indexOf(d.data[selected]), d.data[selected]]//,d, selected, drowDropdowns[selected], d.data[selected]]
-        // console.log(tmp)
-        return colorArr(arr.indexOf(d.data[selected]))
-    }
+    
+    
     
     /* Add SVG */
     var svg = d3.select("#chart").append("svg")
@@ -43,10 +75,10 @@ var drow = (data_, linetype) => {
     
         // /* Add sieverts into SVG */
         
-        var line = d3.line()
-            .curve(step ? d3.curveStepBefore : basis ? d3.curveBasis : d3.curveLinear )
-            .x((d, i) => xScale(i))
-            .y((d, i) => yScale(d));
+    var line = d3.line()
+        .curve(step ? d3.curveStepBefore : basis ? d3.curveBasis : d3.curveLinear )
+        .x((d, i) => xScale(i))
+        .y((d, i) => yScale(d));
         
     
     
@@ -85,7 +117,8 @@ var show = (d) => {
     
     // alert(d.data.Experience)
     var msg = '';
-    ["1.2 Title","Experience","FragmentID","NarrID","2.1 Are you related to/do you know either the victim or the perpetrator?","2.2 How is the victim related to the perpetrator?","2.3 What is the age category of the victim?","2.5 What is the gender of the perpetrator?","2.6 Where did this situation happen?","2.6 Other","2.8 Where did this situation happen?","3.1 Did you act/do something about this situation?","3.5 Have you contacted any services about this situation?","3.5 Other","4.1 The outcome of this situation for the victim was…","4.2 Situations like these…","5.1 If you were to witness a similar situation in the future you would…","6.5 Education","6.1 Gender","6.2 Age","6.6 Marital status","6.3 Ethnicity","6.3 Other","6.7 Monthly income","6.4 Attendance of religious services","6.8 Region of residence","6.11 Region originally from","6.11 Other","6.9 Residence","6.10 How long have you lived here for?"].forEach(m => {
+    ["Title","Experience"]//,"FragmentID","NarrID","Are you related to/do you know either the victim or the perpetrator?","How is the victim related to the perpetrator?","What is the age category of the victim?","2.5 What is the gender of the perpetrator?","2.6 Where did this situation happen?","2.6 Other","2.8 Where did this situation happen?","3.1 Did you act/do something about this situation?","3.5 Have you contacted any services about this situation?","3.5 Other","4.1 The outcome of this situation for the victim was…","4.2 Situations like these…","5.1 If you were to witness a similar situation in the future you would…","6.5 Education","6.1 Gender","6.2 Age","6.6 Marital status","6.3 Ethnicity","6.3 Other","6.7 Monthly income","6.4 Attendance of religious services","6.8 Region of residence","6.11 Region originally from","6.11 Other","6.9 Residence","6.10 How long have you lived here for?"]
+        .forEach(m => {
         msg += m + ': ' + d.data[m] + '\n'
     })
     alert(msg)
@@ -93,31 +126,101 @@ var show = (d) => {
 }
      
 var hover = (d) => {
-    d3.selectAll('.line').filter(line => line.data.NarrID != d.data.NarrID)
+    d3.selectAll('.line').filter(line => line.data.FragmentID != d.data.FragmentID)
                     .style('opacity', .01);
-    d3.selectAll('.line').filter(line => line.data.NarrID == d.data.NarrID)
+    d3.selectAll('.line').filter(line => line.data.FragmentID == d.data.FragmentID)
                     .style('stroke-width', 2);
+    var dotsOnLine = document.getElementsByClassName('dot-on-line')
+    for(var dotIndex; dotIndex < dotsOnLine.length; dotIndex++){
+        if(dotsOnLine[dotIndex].getAttribute('nar-id') == d.data.FragmentID){
+            dotsOnLine[dotIndex].classList.remove('hide')
+        } else {
+            dotsOnLine[dotIndex].classList.add('hide')
+        }
+    }
+
+    Object.keys(d.data).forEach(key => {
+        if(drowDropdowns[key]){
+            // console.log(key)
+            opts = drowDropdowns[key].dom.getElementsByClassName('select-option')
+            for(var optIndex = 0; optIndex < opts.length; optIndex++){
+                if(opts[optIndex].innerHTML == d.data[key]){
+                    opts[optIndex].classList.add('higlight')
+                } else {
+                    opts[optIndex].classList.remove('higlight')
+                }
+            }
+        }
+    })
+                    
 }
 var out = (d) => {
     d3.selectAll(".line").style('opacity', .9).style('stroke-width', .7);
+    opts = document.getElementsByClassName('select-option')
+    for(var optIndex = 0; optIndex < opts.length; optIndex++){
+        opts[optIndex].classList.remove('higlight')
+    }
+    var dotsOnLine = document.getElementsByClassName('dot-on-line')
+    for(var dotIndex; dotIndex < dotsOnLine.length; dotIndex++){
+        dotsOnLine[dotIndex].classList.remove('hide')
+    }
 }
 
 const mapToChartData = row => ({ lineData: step ? 
-    [+row['3.6 You did not act/do something because you…-Feared for your safety'],+row['3.6 You did not act/do something because you…-Feared for your safety'],+row['3.6 You did not act/do something because you…-Believed it was not your job/place'],+row['3.6 You did not act/do something because you…-Did not know how to help'],+row['3.2 You did something because you-Could rely on support of public services'],+row['3.2 You did something because you-Felt responsible for dealing with it'],+row['3.2 You did something because you-Knew what to do/how to help'],+row['3.7 You decided to act/not act the way you did because-You did not have a choice'],+row['3.7 You decided to act/not act the way you did because-To you it was the right thing to do'],+row['3.7 You decided to act/not act the way you did because-It was an appropriate/acceptable thing to do']] : 
-    [+row['3.6 You did not act/do something because you…-Feared for your safety'],+row['3.6 You did not act/do something because you…-Believed it was not your job/place'],+row['3.6 You did not act/do something because you…-Did not know how to help'],+row['3.2 You did something because you-Could rely on support of public services'],+row['3.2 You did something because you-Felt responsible for dealing with it'],+row['3.2 You did something because you-Knew what to do/how to help'],+row['3.7 You decided to act/not act the way you did because-You did not have a choice'],+row['3.7 You decided to act/not act the way you did because-To you it was the right thing to do'],+row['3.7 You decided to act/not act the way you did because-It was an appropriate/acceptable thing to do']],
+    [+row["41DysfunctionalInstitutions"],
+    +row["41DysfunctionalInstitutions"],
+    +row["41LowLivingStandards"],
+    +row["41LackHhealthyEnvironment"],
+    +row["42BetterQualityLife"],
+    +row["42BetterEducationChildren"],
+    +row["42BetterAccessHealthSocialServices"],
+    +row["43GlobalNetwork"],
+    +row["43NetworkAcquaintances"],
+    +row["43NumerousFacilitatingFactors "]] : 
+    [+row["41DysfunctionalInstitutions"],
+    +row["41LowLivingStandards"],
+    +row["41LackHhealthyEnvironment"],
+    +row["42BetterQualityLife"],
+    +row["42BetterEducationChildren"],
+    +row["42BetterAccessHealthSocialServices"],
+    +row["43GlobalNetwork"],
+    +row["43NetworkAcquaintances"],
+    +row["43NumerousFacilitatingFactors "]],
     data: row})
 
 let globalData
-d3.csv('./undp_ipv_ge_6Sept19-undp_ipv_ge_21Aug19.csv')
+d3.csv('./Macedonia Data FINAL - Sheet 1.csv')
     .then(csv => {
-        globalData = csv
-        buildDropdowns(csv)
-        drow(csv.map(mapToChartData))
+        csv_nonum = []
+        csv.forEach(row => {
+            now_nonum = {}
+            Object.keys(row).forEach(colname => {
+                // now_nonum[colname.replace(/^\d+\.\d+ /gi , '')] = row[colname] 
+                now_nonum[colname] = row[colname] 
+            })
+            csv_nonum.push(now_nonum)
+        }) 
+        globalData = csv_nonum
+        drowFilters(globalData)
+        // globalData = csv
+        buildDropdowns(csv_nonum)
+        drow(csv_nonum.map(mapToChartData))
     })
 
 
 const buildDropdowns = (data) => {
-    const columns = [ '2.1 Are you related to/do you know either the victim or the perpetrator?','2.2 How is the victim related to the perpetrator?','2.3 What is the age category of the victim?','2.5 What is the gender of the perpetrator?','2.6 Where did this situation happen?','2.8 Where did this situation happen?','3.1 Did you act/do something about this situation?','3.5 Have you contacted any services about this situation?','4.1 The outcome of this situation for the victim was…','4.2 Situations like these…','5.1 If you were to witness a similar situation in the future you would…','6.5 Education','6.1 Gender','6.2 Age','6.6 Marital status','6.3 Ethnicity','6.7 Monthly income','6.4 Attendance of religious services','6.8 Region of residence','6.11 Region originally from','6.9 Residence','6.10 How long have you lived here for?' ]
+    const columns = [ 'Are you related to/do you know either the victim or the perpetrator?',
+        'How is the victim related to the perpetrator?',
+        'What is the age category of the victim?',
+        'What is the gender of the perpetrator?',
+        'Where did this situation happen?',
+        'Where did this situation happen?',
+        'Did you act/do something about this situation?',
+        'Have you contacted any services about this situation?',
+        'The outcome of this situation for the victim was…',
+        'Situations like these…',
+        'If you were to witness a similar situation in the future you would…',
+        'Education','Gender','Age','Marital status','Ethnicity','Monthly income','Attendance of religious services','Region of residence','Region originally from','Residence','How long have you lived here for?' ]
     options = {}
     columns.forEach(col => options[col] = [])
     data.forEach( row => {
@@ -129,7 +232,7 @@ const buildDropdowns = (data) => {
     })
     
     var colorizer = document.getElementsByName('coloryze')[0]
-    columns.forEach(opt => {
+    colorizeColumns.forEach(opt => {
         var option = document.createElement('option')    
         option.setAttribute("value", opt)
         option.innerHTML = opt
@@ -140,12 +243,14 @@ const buildDropdowns = (data) => {
         sss.innerHTML = ''
         var coloryzess = document.getElementsByName('coloryze')[0].value
         if(coloryzess && coloryzess != 'None' && drowDropdowns[coloryzess]){
-            var ppppt = drowDropdowns[coloryzess].options
-            for(var i = 0; i < ppppt.length; i++){
-                let li = document.createElement("li")
-                li.innerText = ppppt[i].innerText
-                sss.appendChild(li)
-            }
+            // var ppppt = 
+            document.getElementsByClassName('col-codes')[0].classList.remove('col-codes')
+            drowDropdowns[coloryzess].dom.classList.add('col-codes')// .getElementsByClassName('selected-option')
+            // for(var i = 0; i < ppppt.length; i++){
+            //     let li = document.createElement("li")
+            //     li.innerText = ppppt[i].innerText
+            //     sss.appendChild(li)
+            // }
         }
         
         filter()
@@ -166,46 +271,83 @@ const buildDropdowns = (data) => {
     })
     
 
-    columns.forEach(col => {
-        drowDropdown(col, options[col])
-    })
+
+    var sels = document.getElementsByClassName('select-title')
+    for(var kk = 0; kk < sels.length; kk++){
+        drowDropdowns[sels[kk].innerText] = { dom: sels[kk].parentNode, values: [] }
+        var opts = sels[kk].parentNode.getElementsByClassName('select-option');
+        for(var optIndex = 0; optIndex < opts.length; optIndex++){
+            opts[optIndex].addEventListener('click', e => {
+                var drpd = drowDropdowns[e.target.parentNode.getElementsByClassName('select-title')[0].innerText]
+                if(drpd.values.indexOf(e.target.innerText) === -1){
+                    drpd.values.push(e.target.innerText)
+                    e.target.classList.add('option-selected')
+                } else {
+                    drpd.values.splice(drpd.values.indexOf(e.target.innerText), 1)
+                    e.target.classList.remove('option-selected')
+                }
+                filter()
+            })
+
+        }
+    }
+    
 }
 
-
+// const drowDropdownDoms = {}
 const drowDropdowns = {}
-const drowDropdown = (col, options) => {
 
-    var form = document.getElementsByTagName('form')[0]
-    var select= document.createElement('select')
-    
-    select.setAttribute("multiple", "multiple")
-    
-    // console.log(select)
-    form.appendChild(select)
-    
-    options.forEach(opt => {
-        var option = document.createElement('option')    
-        option.setAttribute("value", opt)
-        option.innerHTML = opt
-        select.appendChild(option)
-    })
-    document.getElementById("clear").addEventListener('click', () => {
-        Object.keys(drowDropdowns).forEach(dn => {
-            $(drowDropdowns[dn]).multiselect("clearSelection"); 
-        })    
-        filter();
-    })
+document.getElementById("clear").addEventListener('click', () => {
+    var opts = document.getElementsByClassName('select-option');
+    for(var optIndex = 0; optIndex < opts.length; optIndex++){
+        opts[optIndex].classList.remove('option-selected')
+    }
+    Object.keys(drowDropdowns).forEach(dn => {
+        drowDropdowns[dn].values = []; 
+    })    
+    filter();
+})
 
-    $(select).multiselect({includeSelectAllOption: false, nonSelectedText: col, enableFiltering: false , enableClickableOptGroups: true});
-
-    $(select).on('change', filter)
-    drowDropdowns[col] = select;
-}
 
 const filter = () => {
     drow(Object.keys(drowDropdowns)
         .reduce((globalDataReduced,drowDropdownName) => 
-            globalDataReduced.filter(row => !$(drowDropdowns[drowDropdownName]).val() || $(drowDropdowns[drowDropdownName]).val().indexOf(row[drowDropdownName]) !== -1)
+            globalDataReduced.filter(row => drowDropdowns[drowDropdownName].values.length == 0 || drowDropdowns[drowDropdownName].values.indexOf(row[drowDropdownName]) !== -1)
         , globalData)
         .map(mapToChartData))
+}
+
+const uniqueValues = {}
+const colorizeColumns = []
+const drowFilters = (dataRows) => {
+    cols = Object.keys(dataRows[0])
+    cols.forEach(col => uniqueValues[col] = [])
+    
+    dataRows.forEach(row => {
+        cols.forEach(col => {
+            if ( uniqueValues[col].indexOf(row[col]) === -1) uniqueValues[col].push(row[col])
+        })
+    })
+    
+    cols.forEach(col => {
+        if (col.indexOf('Other') !== -1 || col.indexOf('3.4') !== -1 || col.indexOf('6.3') !== -1) return
+        let sec = (col.indexOf('4.4') === 0 || col.indexOf('4.5') === 0) ? 1 : col.indexOf('2.') === 0 ? 2 : col.indexOf('3.') === 0 ? 3 : col.indexOf('6.') === 0 ? 4 : !1
+        if(!sec) return
+        colorizeColumns.push(col)
+        let cont = document.createElement('div')
+        cont.setAttribute('class', "select-group")
+        let titl = document.createElement('div')
+        titl.setAttribute('class', "select-title")
+        titl.innerText = col
+        cont.appendChild(titl)
+        uniqueValues[col].forEach(val => {
+            let opt = document.createElement('div')
+            opt.setAttribute('class', "select-option")
+            opt.innerText = val
+            cont.appendChild(opt)
+        })
+        document.getElementById("sec-" + sec).appendChild(cont)
+    })
+    
+
 }
